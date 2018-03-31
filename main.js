11cf07ca541fe6920e135d68dc4d5227218dad0a
Jhonatan Faber <jfaber@farom.com>
2018-03-31 12:46:32 +0100

    footer done

--- main.js
+++ main.js
@@ -1,3 +1,28 @@
-$("#myButton").click(function(e){
-   $("#mySelect").show('slow');//or just show instead of toggle
-});
+initMap();
+
+
+function initMap() {
+  $(".dropdown p").hide();
+  $(".wrapper__image img").hide();
+  var uluru = { lat: 40.416775, lng: -3.70379 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 11,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
+
+
+
+function dropdown(id) {
+  $(".dropdown p").hide();
+  $(".wrapper__image img").hide();
+  if ($(id).is("#CreativeService")) {
+    $(".wrapper__image img").show();
+  }
+  $(id).show();
+}
+
